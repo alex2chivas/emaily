a4b499dd04a59cef030b69308d627a9aeeeecc8a
alex2chivas <alex2chivas@hotmail.com>
2020-07-26 13:29:52 -0700

    added react router dom setup

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+const Header = () => <h2>Hello from header</h2>;
+const HeaderDash = () => <h2>Hello from Dash</h2>;
 
 const App = () => {
-	return <div>Hello World</div>;
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route path='/' exact component={Header} />
+				<Route path='/headerdash' exact component={HeaderDash} />
+			</Switch>
+		</BrowserRouter>
+	);
 };
 
 export default App;
