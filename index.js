780fa4962471fa27ff8d4f0266044ea8f7b7cf19
alex2chivas <alex2chivas@hotmail.com>
2020-07-27 21:46:25 -0700

    added stripe token to the backend to manage credit charge and update the credits for the user

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const { mongoURI, cookieKey } = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -10,10 +11,14 @@ mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use(
 	cookieSession({
 		maxAge: 30 * 24 * 60 * 60 * 1000,
-		keys: [ cookieKey ]
+		keys: [
+			cookieKey
+		]
 	})
 );
 
@@ -21,6 +26,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
