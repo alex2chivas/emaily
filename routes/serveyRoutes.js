b1ef10b799b818b636ab217795d1c22aeb0b2475
alex2chivas <alex2chivas@hotmail.com>
2020-07-30 13:33:51 -0700

    added serveryRoute to handle the survery request from the client side

--- routes/serveyRoutes.js
+++ routes/serveyRoutes.js
@@ -1,6 +1,21 @@
+const moment = require('moment');
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 
+const Survey = mongoose.model('surveys');
+
 module.exports = app => {
-	app.post('/api/serveys', requireLogin, requireCredits, (req, res) => {});
+	app.post('/api/serveys', requireLogin, requireCredits, (req, res) => {
+		const { title, subject, body, recipients } = req.body;
+
+		const survey = new Survey({
+			title,
+			subject,
+			body,
+			recipients: recipients.split(',').map(email => ({ email: email.trim() })),
+			_user: req.user.id,
+			dateSent: moment().format('llll')
+		});
+	});
 };
