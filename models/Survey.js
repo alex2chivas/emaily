0b1651bd1e65784a8a293028b618d2902c408f6c
alex2chivas <alex2chivas@hotmail.com>
2020-07-29 21:33:02 -0700

    added surverySchema

--- models/Survey.js
+++ models/Survey.js
@@ -16,7 +16,13 @@ const serveySchema = new Schema({
 	no: {
 		type: Number,
 		default: 0
-	}
+	},
+	_user: {
+		type: Schema.Types.ObjectId,
+		ref: 'User'
+	},
+	dateSent: Date,
+	lastResponded: Date
 });
 
 mongoose.model('surveys', serveySchema);
