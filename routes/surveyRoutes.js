c0aff29b914925e3abd3d96cdd72fdf796f10bad
alex2chivas <alex2chivas@hotmail.com>
2020-07-31 20:33:31 -0700

    finished the back end route handler

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -8,7 +8,11 @@ const surveytemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.get('/api/surveys/thanks', (req, res) => {
+		res.send('Thanks so much for filling out the survey');
+	});
+
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
 		const survey = new Survey({
@@ -22,6 +26,16 @@ module.exports = app => {
 
 		// Great place to end an email
 		const mailer = new Mailer(survey, surveytemplate(survey));
-		mailer.send();
+
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };
