393987b27bf7b53e68ecbe7bb7a8837393136c1c
alex2chivas <alex2chivas@hotmail.com>
2020-08-03 15:09:04 -0700

    setup to extract the webhook from sendgrid

--- config/keys.js
+++ config/keys.js
@@ -5,3 +5,5 @@ if (process.env.NODE_ENV === 'production') {
 	// we are in development
 	module.exports = require('./dev');
 }
+
+// Notes for using local webhooks setup on sendGrid: run npx ngrok http 5000
